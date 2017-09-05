976e147d0adc279458bf84bd517f700c4b427333
weblgdong <weblgdong@gmail.com>
2017-09-06 07:06:43 +0800

    update

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,9 +1,10 @@
 import jsonp from 'common/js/jsonp';
 import {commonParams, options} from './config';
+import axios from 'axios';
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
-
+  // 用于将所有可枚举的属性的值从一个或多个源对象复制到目标对象。它将返回目标对象。
   const data = Object.assign({}, commonParams, {
     platform: 'h5',
     uin: 0,
@@ -11,4 +12,26 @@ export function getRecommend() {
   });
 
   return jsonp(url, data, options);
+}
+
+export function getDiscList() {
+  const url = '/api/getDiscList';
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: 'json'
+  });
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data);
+  });
 }
