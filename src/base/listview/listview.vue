9245b6185eb8306ed9e77540b590c4afb0053a2e
weblgdong <weblgdong@gmail.com>
2017-09-15 23:36:30 +0800

    updata

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -26,14 +26,21 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll';
   import {getData} from 'common/js/demo';
-
+  import Loading from 'base/loading/loading';
   const ANCHOR_HEIGHT = 18;
+  const TITLE_HEIGHT = 30;
 
   export default {
     created() {
@@ -45,7 +52,8 @@
     data() {
       return {
         scrollY: -1,
-        currentIndex: 0
+        currentIndex: 0,
+        diff: -1
       };
     },
     props: {
@@ -59,6 +67,12 @@
         return this.data.map((group) => {
           return group.title.substr(0, 1);
         });
+      },
+      fixedTitle() {
+        if (this.scrollY > 0) {
+          return '';
+        }
+        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : '';
       }
     },
     methods: {
@@ -80,6 +94,15 @@
         this.scrollY = pos.y;
       },
       _scrollTop(index) {
+        if (!index && index !== 0) {
+          return;
+        }
+        if (index < 0) {
+          index = 0;
+        } else if (index > this.listHeight.length - 2) {
+          index = this.listHeight.length - 2;
+        }
+        this.scrollY = -this.listHeight[index];
         this.$refs.listview.scrollElement(this.$refs.listGroup[index], 0); // 0联动动画时间
       },
       _calculateHeight() {
@@ -113,15 +136,25 @@
           let height2 = listHeight[i + 1];
           if (-newY >= height1 && -newY < height2) {
             this.currentIndex = i;
+            this.diff = height2 + newY;
             return;
           }
         }
         // 当滚动到底部，且-newY大于最后一个元素的上限
         this.currentIndex = listHeight - 2;
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0;
+        if (this.fixedTop === fixedTop) {
+          return;
+        }
+        this.fixedTop = fixedTop;
+        this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`;
       }
     },
     components: {
-      Scroll
+      Scroll,
+      Loading
     }
   };
 </script>
